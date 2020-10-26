385a8d0f7d35750ab043aee336cddc4d42f94de0
Ali Sulieman <ahsuliem@uno.edu>
2020-10-26 13:21:44 -0500

    building an app using react native also the app will use the CURD api

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-dom';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Login from "./component/logger/Login"
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <view>
+        <Login>
+
+        </Login>
+      </view>
+    )
+  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    backgroundColor: '#00ffff'
   },
 });
---
+++ component/logger/Login.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, Image, TextInput, ImageBackground } from 'react-native';
+import { Card } from 'react-native-elements'
+import { round } from 'react-native-reanimated';
+export default class login extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <ImageBackground style={styles.image} source={require('../../images/greece.png')}>
+                    <Card containerStyle={{
+                        width: 300,
+                        height: 300,
+                        borderColor: 'grey',
+                        backgroundColor: 'grey',
+                        round: 'true'
+                    }
+                    }>
+                        <Card.Title style={{ color: 'blue' }}>Sign In</Card.Title>
+                    </Card>
+                </ImageBackground>
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column"
+    },
+    paragraph: {
+        margin: 24,
+        marginTop: 0,
+        fontSize: 14,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#34495e',
+    },
+    logo: {
+        height: 128,
+        width: 128,
+        marginBottom: 80
+    },
+    userinput: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        height: 40,
+        borderColor: 'grey',
+        borderWidth: 3,
+        width: 190,
+        marginBottom: 140
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+        height: 812,
+        width: 812
+    },
+});
